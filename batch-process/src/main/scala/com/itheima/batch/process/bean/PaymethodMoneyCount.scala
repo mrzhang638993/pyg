e1e6ea41d6042483fae6da0205a58ad835ef612a
mrzhang638993 <1446719386@qq.com>
2020-11-07 09:48:58 +0800

    从支付方式角度进行数据统计操作实现

--- batch-process/src/main/scala/com/itheima/batch/process/bean/PaymethodMoneyCount.scala
+++ batch-process/src/main/scala/com/itheima/batch/process/bean/PaymethodMoneyCount.scala
@@ -3,4 +3,9 @@ package com.itheima.batch.process.bean
 /**
  * 支付方式角度进行数据统计操作实现
  * */
-case class PaymethodMoneyCount()
+case class PaymethodMoneyCount(
+                                var payMethod:String, // 支付方式
+                                var date:String,      //  日期
+                                var moneyCount:Double, //  订单金额
+                                var count:Long         // 订单总数
+                              )
--- batch-process/src/main/scala/com/itheima/batch/process/task/PaymethodMoneyCountTask.scala
+++ batch-process/src/main/scala/com/itheima/batch/process/task/PaymethodMoneyCountTask.scala
@@ -1,13 +1,33 @@
 package com.itheima.batch.process.task
 
-import com.itheima.batch.process.bean.OrderRecord
+import com.itheima.batch.process.bean.{ OrderRecordWide, PaymethodMoneyCount}
 import org.apache.flink.api.scala.DataSet
+import org.apache.flink.api.scala._
 
 object PaymethodMoneyCountTask {
     /**
      * 从支付方式的角度进行数据的统计操作
      * */
-    def process(orderRecord: DataSet[OrderRecord]):DataSet[]={
+    def process(orderRecord: DataSet[OrderRecordWide]):DataSet[PaymethodMoneyCount]={
+       // 转换
+        val methodValue: DataSet[PaymethodMoneyCount] = orderRecord.flatMap {
+            orderData => {
+                List(
+                    PaymethodMoneyCount(orderData.payMethod, orderData.yearMonthDay, orderData.payAmount.toDouble, 1L),
+                    PaymethodMoneyCount(orderData.payMethod, orderData.yearMonth, orderData.payAmount.toDouble, 1L),
+                    PaymethodMoneyCount(orderData.payMethod, orderData.year, orderData.payAmount.toDouble, 1L)
+                )
+            }
+        }
+        // 分组
+        val dateGroupValue: GroupedDataSet[PaymethodMoneyCount] = methodValue.groupBy(_.date)
+        // 聚合
+        val reduceValue: DataSet[PaymethodMoneyCount] = dateGroupValue.reduce((priv, next) => PaymethodMoneyCount(priv.payMethod, priv.date, priv.moneyCount + next.moneyCount, priv.count + next.count))
+        // 落地到hbase中
+        val tableName:String=""
+        val clfName=""
 
+        //HBaseUtil.putMapData(tableName,clfName,)
+        null
     }
 }
