b0bd9b9a933089f521a10bad5df114ffa739e025
mrzhang638993 <1446719386@qq.com>
2021-06-27 21:40:51 +0800

    完成渠道的累计操作

--- real-process/src/main/scala/com/itheima/realprocess/task/JDBCSinkDemo.scala
+++ real-process/src/main/scala/com/itheima/realprocess/task/JDBCSinkDemo.scala
@@ -1,31 +1,37 @@
-public static void main(String[] args) throws Exception {
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+package com.itheima.realprocess.task
 
-        //准备数据源
-        List<Product> list = new ArrayList<>();
-        list.add(new Product("跑鞋", "耐克", 998));
-        list.add(new Product("短裤", "李宁", 119));
-        list.add(new Product("袜子", "耐克", 68));
-        list.add(new Product("西服", "海澜之家", 1000));
-        DataStreamSource<Product> dataStreamSource = env.fromCollection(list);
+import org.apache.flink.api.scala.createTypeInformation
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.functions.sink.{RichSinkFunction, SinkFunction}
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
 
-        String url = "jdbc:mysql://localhost:3306/flink_data?useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT";
-        String sql = "insert into t_product(name, brand, price) values (?,?,?)";
-        dataStreamSource.addSink(JdbcSink.sink(sql,
-                new JdbcStatementBuilder<Product>() {
-                    @Override
-                    public void accept(PreparedStatement ps, Product product) throws SQLException {
-                        ps.setString(1, product.getName());
-                        ps.setString(2, product.getBrand());
-                        ps.setDouble(3, product.getPrice());
-                    }
-                },
-                new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
-                        .withUrl(url)
-                        .withDriverName("com.mysql.jdbc.Driver")
-                        .withUsername("root")
-                        .withPassword("123456")
-                        .build()));
+import java.sql.{Connection, PreparedStatement}
 
-        env.execute("jdbc data sink");
-    }
+object JDBCSinkDemo {
+  def main(args: Array[String]): Unit = {
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    //准备数据源
+    val tuples = List(("跑鞋", "耐克", 998), ("短裤", "李宁", 119), ("袜子", "耐克", 68), ("西服", "海澜之家", 1000))
+    val dataValue: DataStream[(String, String, Int)] = env.fromCollection(tuples)
+
+    dataValue.addSink(new RichSinkFunction[(String, String, Int)] {
+      val conn:Connection=null
+      val ps:PreparedStatement=null
+      // 做全局的配置操作和实现管理
+      override def open(conf: Configuration): Unit = {
+        super.open(conf)
+        val  url = "jdbc:mysql://localhost:3306/flink_data?useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT";
+        val  sql = "insert into t_product(name, brand, price) values (?,?,?)";
+        Class.forName("")
+        //DriverManager.registerDriver()
+      }
+      override def invoke(value: (String, String, Int), context: SinkFunction.Context[_]): Unit ={
+
+      }
+      override def close(): Unit ={
+        // 执行资源关闭的方式和方法
+      }
+    })
+    env.execute("jdbc data sink");
+  }
+}
