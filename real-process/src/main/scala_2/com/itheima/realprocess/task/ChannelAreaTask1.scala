a5712f4777838859c413692aaed3c9e717ff18dd
mrzhang638993 <1446719386@qq.com>
2021-07-04 10:07:36 +0800

    将数据入库保存到hbase数据库中的

--- real-process/src/main/scala_2/com/itheima/realprocess/task/ChannelAreaTask1.scala
+++ real-process/src/main/scala_2/com/itheima/realprocess/task/ChannelAreaTask1.scala
@@ -1,9 +1,13 @@
 package com.itheima.realprocess.task
 
 import com.itheima.realprocess.bean.{ChannelArea1, ClickLogWide1}
+import com.itheima.realprocess.util.HbaseUtils1
 import org.apache.flink.api.scala.createTypeInformation
+import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.streaming.api.scala.{DataStream, KeyedStream, WindowedStream}
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+
+import scala.collection.mutable
 
 /**
  * 分析渠道地域的行为特征。对应的区分键是渠道以及地域的信息。
@@ -34,7 +38,53 @@ object ChannelAreaTask1 extends BaseTask1[ChannelArea1]{
      value.reduce((priv,next)=>ChannelArea1(priv.channelId,priv.area,priv.date,priv.pv+next.pv,priv.uv+next.uv,priv.newCount+next.newCount,priv.oldCount+next.oldCount))
   }
 
+  //数据落地到hbase中
   override def sinkToHbase(value: DataStream[ChannelArea1]): Unit = {
-
+    value.addSink(new SinkFunction[ChannelArea1]{
+      override def invoke(value: ChannelArea1, context: SinkFunction.Context[_]): Unit = {
+          val tableNameStr:String="channel_area"
+          val clfName:String="info"
+          val rowKey=value.channelId+":"+value.area+":"+value.date
+          val channelIdColumn:String="channelId"
+          val areaColumn:String="area"
+          val dateColumn:String="date"
+          val pvColumn:String="pv"
+          val uvColumn:String="uv"
+          val newCountColumn:String="newCount"
+          val oldCountColumn:String="oldCount"
+          var pvCount:Long=value.pv
+          var uvCount:Long=value.uv
+          var newCount:Long=value.newCount
+          var oldCount:Long=value.oldCount
+          val mapData: Map[String, String] = HbaseUtils1.getMapData(tableNameStr, rowKey, clfName, List(pvColumn, uvColumn, newCountColumn, oldCountColumn))
+          if(mapData!=null){
+            val pvValue: Option[String] = mapData.get(pvColumn)
+            if(!pvValue.isEmpty){
+              pvCount+=pvValue.get.toLong
+            }
+            val uvValue: Option[String] = mapData.get(uvColumn)
+            if(!uvValue.isEmpty){
+              uvCount+=uvValue.get.toLong
+            }
+            val oldCountValue: Option[String] = mapData.get(oldCountColumn)
+            if(!oldCountValue.isEmpty){
+              oldCount+=oldCountValue.get.toLong
+            }
+            val newCountValue: Option[String] = mapData.get(newCountColumn)
+            if(!newCountValue.isEmpty){
+              newCount+=newCountValue.get.toLong
+            }
+          }
+        HbaseUtils1.putMapData(tableNameStr,rowKey,clfName,mutable.Map(
+          channelIdColumn->value.channelId,
+          areaColumn->value.area,
+          dateColumn->value.date,
+          pvColumn->pvCount,
+          uvCount->uvCount,
+          oldCountColumn->oldCount,
+          newCountColumn->newCount
+        ))
+      }
+    })
   }
 }
